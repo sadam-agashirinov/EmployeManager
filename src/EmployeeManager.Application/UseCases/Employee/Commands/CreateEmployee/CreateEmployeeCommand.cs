8ff42739bc9475b3d924db225ace26ae243c4f8b
Sadam Agashirinov <sadam.agashirinov@gmail.com>
2023-01-21 12:31:32 +0300

    Создать сотрудника.

---
+++ src/EmployeeManager.Application/UseCases/Employee/Commands/CreateEmployee/CreateEmployeeCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace EmployeeManager.Application.UseCases.Employee.Commands;
+
+public class CreateEmployeeCommand : IRequest<Guid>
+{
+    public string LastName { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string Patronymic { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public decimal Salary { get; set; }
+    public List<Guid> DepartmentsId { get; set; }
+}
---
+++ src/EmployeeManager.Application/UseCases/Employee/Commands/CreateEmployee/CreateEmployeeCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManager.Application.Interfaces;
+using MediatR;
+
+namespace EmployeeManager.Application.UseCases.Employee.Commands.CreateEmployee;
+
+public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, Guid>
+{
+    private readonly IAppDbContext _dbContext;
+
+    public CreateEmployeeCommandHandler(IAppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Guid> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var employee = new Domain.Entities.Employee()
+        {
+            Id = Guid.NewGuid(),
+            LastName = request.LastName,
+            FirstName = request.FirstName,
+            Patronymic = request.Patronymic,
+            Email = request.Email,
+            Salary = request.Salary,
+            EmployeeDepartmentIds = request.DepartmentsId
+        };
+        
+        await _dbContext.Employees.AddAsync(employee, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return employee.Id;
+    }
+}
